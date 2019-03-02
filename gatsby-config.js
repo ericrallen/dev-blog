300393dae95e8a57d959da825eaca12268f9967d
Eric Allen <eric@ericrallen.com>
2019-03-02 17:38:34 -0500

    added archive to filesystem source config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,13 @@ module.exports = {
         name: "markdown-pages",
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/archive`,
+        name: "markdown-pages",
+      },
+    },
     `gatsby-transformer-remark`,
   ],
 };
