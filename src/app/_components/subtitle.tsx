78bb61c6dbe8be7471ada4e2e8453e96ab425ae4
Eric allen <eric@ericrallen.dev>
2025-04-20 09:47:27 -0400

    feat: give subtitle more options, start with -powered -> -haunted

--- src/app/_components/subtitle.tsx
+++ src/app/_components/subtitle.tsx
@@ -4,9 +4,23 @@ import { useState } from "react";
 import Ciph3rText from "@interwebalchemy/ciph3r-text";
 import { useTimeout } from "usehooks-ts";
 
+const aiModifiers = [
+  "powered",
+  "haunted",
+  "focused",
+  "enabled",
+  "augmented",
+  "transformed",
+  "enhanced",
+  "optimized",
+  "integrated",
+  "accelerated",
+  "transformed",
+];
+
 export function Intro() {
-  const [defaultText, setDefaultText] = useState("haunted");
-  const [targetText, setTargetText] = useState("powered");
+  const [defaultText, setDefaultText] = useState(aiModifiers[0]);
+  const [targetText, setTargetText] = useState<string | undefined>();
   const [hasTransformed, setHasTransformed] = useState(false);
 
   useTimeout(
@@ -16,23 +30,44 @@ export function Intro() {
       const previousTargetText = targetText;
       const previousDefaultText = defaultText;
 
-      setDefaultText(previousTargetText);
-      setTargetText(previousDefaultText);
+      const filteredAiModifiers = aiModifiers.filter(
+        (modifier) =>
+          modifier !== previousTargetText && modifier !== previousDefaultText
+      );
+
+      const randomAiModifier =
+        filteredAiModifiers[
+          Math.floor(Math.random() * filteredAiModifiers.length)
+        ];
+
+      setDefaultText(previousTargetText ?? "");
+      setTargetText(randomAiModifier);
     },
     hasTransformed ? 2500 : null
+  );
+
+  useTimeout(
+    () => {
+      setTargetText(aiModifiers[1]);
+    },
+    hasTransformed ? null : 2500
   );
 
   return (
     <h4 className="text-4xl text-gray-400">
       Human-centered software for an AI-
-      <Ciph3rText
-        defaultText={defaultText}
-        targetText={targetText}
-        action="transform"
-        onFinish={() => {
-          setHasTransformed(true);
-        }}
-      />{" "}
+      {targetText ? (
+        <Ciph3rText
+          defaultText={defaultText}
+          targetText={targetText}
+          action="transform"
+          onFinish={() => {
+            setHasTransformed(true);
+          }}
+        />
+      ) : (
+        defaultText
+      )}{" "}
       world.
     </h4>
   );
